a42c5b865babceda0cfc69ecdf6fc4bbffc6a7fc
varuntayur <varuntayur@gmail.com>
2014-10-09 15:19:35 +0530

    fixes to login screen, new launcher icon

--- app/src/main/java/com/vtayur/intellivote/VoteActivity.java
+++ app/src/main/java/com/vtayur/intellivote/VoteActivity.java
@@ -1,11 +1,15 @@
 package com.vtayur.intellivote;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class VoteActivity extends ActionBarActivity {
 
@@ -22,6 +26,16 @@ public class VoteActivity extends ActionBarActivity {
 
         TextView tv = (TextView) findViewById(R.id.tvProgramName);
         tv.setText("Channel " + position + " - Program " + (position + 1) + "\n" + "(" + "100k votes)");
+
+        Button btn = (Button) findViewById(R.id.buttonVoteSubmit);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(VoteActivity.this, "Your vote was registered. Thank You!", Toast.LENGTH_SHORT).show();
+                setResult(Activity.RESULT_OK);
+                finish();
+            }
+        });
 
     }
 
